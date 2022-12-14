5008c50d035436bc90cc2021c2aa77d12a594bed
SuperDork <bearbaier@gmail.com>
2022-12-13 20:49:25 -0600

    scroll bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,19 @@ window.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
