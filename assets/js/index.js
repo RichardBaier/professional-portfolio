ec7ca9cc8ed1163ed1ee67f50ee830a87f4e93b0
SuperDork <bearbaier@gmail.com>
2022-12-30 16:29:06 -0600

    finished the js for project slides

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 let menuToggle = document.querySelector('.menuToggle');
 let navigation = document.querySelector('.navigation');
 let list = document.querySelectorAll('.list');
+var x = window.matchMedia("(min-width: 1100px)")
 
 menuToggle.onclick = function() {
   navigation.classList.toggle('active');
@@ -18,3 +19,42 @@ list.forEach((item) =>
   max: 25,
   speed: 400
  });
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  } else {
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      slidesPerGroup: 1,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+}
+
+myFunction(x);
+x.addListener(myFunction);
